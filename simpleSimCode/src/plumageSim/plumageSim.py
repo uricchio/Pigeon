205268be769fe0e6d51a77d704d88ae193c2ba52
Lawrence Uricchio <telemacher@Lawrences-iMac.local>
2023-10-30 14:35:38 -0400

    updated model to correct qEq

--- simpleSimCode/src/plumageSim/plumageSim.py
+++ simpleSimCode/src/plumageSim/plumageSim.py
@@ -13,25 +13,25 @@ from collections import defaultdict
 
 class SimulatePlumage():
 
-    def __init__(self,N=5000,s_a=-0.01,s_A=-0.02,p=0.5,S=0.01,alpha=26.4,beta=0.31,x_T=80,dx_T=0.5):
+    def __init__(self,N=100000,s_a=-0.01,s_A=-0.1,p=0.9,S=0.01,alpha=12.09,beta=0.39,x_T=38,dx_T=1):
         self.N = N
-        self.s_A = s_A
         self.s_a = s_a
         self.p = p
         self.S = S
         self.a = alpha
         self.b = beta
-        self.d = 0.32
+        self.d = 0.1
         self.dx_T = dx_T
         self.x_T = x_T
         self.t = 0
         self.d_a = 0
         self.d_A = 0
         self.getDays()
+        self.s_A = self.get_sA(p)
 
     # function to do W-F model given current population status
     def nextGen(self):
-      
+        
         self.getDays()       
    
         # get fitnesses of phenotypes
@@ -41,13 +41,17 @@ class SimulatePlumage():
         # get expected frequencies in next generation and sample from binomial
         fExp = (self.p*Waa + (self.p**0.5)*(1-self.p**0.5)*WA)/(self.p*Waa + (1-self.p)*WA)
 
+        if fExp < 0:
+            fExp = 0. # 1./(self.N*2)
+        elif fExp > 1:
+            fExp = 1. #1-1./(self.N*2)
+
         f = np.random.binomial(2*self.N, fExp)/(self.N*2)
         self.p = f**2
 
+        print(self.p,fExp**2,self.qEq(),self.p*(self.x_T-self.dx_T)+(1-self.p)*(self.x_T))
+
         self.t += 1
-
-        print(self.p,-1.*self.S*(self.da-self.dA)/self.s_a)
-
         return
   
     def TotalDgTmax(self,x_T):
@@ -56,6 +60,8 @@ class SimulatePlumage():
         b = self.b
         d = self.d
         t = self.t
+        if t > 100:
+            t = 100
 
         const= a + b*d*t
         num = (x_T**(const**2 / a)) * ((b*const/a)**(const**2 / a)) * ((x_T * b * const/a)**(-(const**2)/a))
@@ -63,14 +69,22 @@ class SimulatePlumage():
 
         denom = gamma(const**2/a)
 
-        return (1 + np.exp(num/denom))
+        return (1 + num/denom)
 
     # function to select new d_A and d_a
     def getDays(self):
  
-        self.dA = 365*self.TotalDgTmax(self.x_T)
-        self.da = 365*self.TotalDgTmax(self.x_T-self.dx_T)
+        self.d_A = 365*self.TotalDgTmax(self.x_T)
+        self.d_a = 365*self.TotalDgTmax(self.x_T-self.dx_T)
 
         return
 
-   
+    def qEq(self):
+    
+        return (self.s_A - ((1-self.S)**self.d_a-(1-self.S)**self.d_A))/(self.s_A+self.s_a)
+
+    def get_sA(self,q):
+
+        return (-q*self.s_a-(1-self.S)**self.d_a+(1-self.S)**self.d_A)/(q-1)
+        
+
