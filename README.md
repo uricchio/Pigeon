3222cf4233283c3754c8219ca01c3dbf9dee9a03
Lawrence Uricchio <uricchio@telemacher.local>
2025-05-16 12:10:36 -0400

    updated README

--- README.md
+++ README.md
@@ -4,9 +4,18 @@ Code in this directory corresponds to the analyses presented in Wright-Lichter e
 "Multimodal selection and the evolution of plumage on a changing planet." Instructions 
 for generating each of the figures are included below.
 
+## Code installation
+
+The code uses a `setup.py` file to install. If you have `Python3` installed, you should be able to install the source by 
+running `python setup.py install` at the command line. If you do not have root priveleges, you can try `python setup.py install --user`.
+
+There is a second set of source files in the directory `simpleSimCode`. To install this software, `cd` into this directory and run the 
+same command (`python setup.py install`) as above.
+
 ## Main figures
 
 ### Figure 1
+To make Figure 1, first run `./makeAnalyticalFigData.sh`, which  
 
 ## Supplemental figures
 
--- makeFigures.sh
+++ makeFigures.sh
@@ -1,31 +1,23 @@
-# tomorrow
-# remake figures for sims with correcion added
-# fix calc of d_A-d_a in new pigeon sim and see if that gets it to work
-# CHECK 1. make phoenix temp dist figure with best fit dist for summer months (skew normal)
-# CHECK 2. remake the figure with inc and decreasing (d_A-d_a) proportions of the dist
-# CHECK 3. make sim figure showing how outcomes can range
-# CHECK 4. work on pigeon sims (more realistic version of earlier ones)
-
 # make temp data
-#python simpleSimCode/scripts/tempParams.py > tempData/Phoenix.TMax.params.txt
-#python simpleSimCode/scripts/tempParamsNY.py > tempData/NewYork.TMax.params.txt
+python simpleSimCode/scripts/tempParams.py > tempData/Phoenix.TMax.params.txt
+python simpleSimCode/scripts/tempParamsNY.py > tempData/NewYork.TMax.params.txt
 
-# make plots of d_A-d_a
-#python simpleSimCode/scripts/tempAbove_x_T.py  > tempData/diff_dA_da.x_T.txt
-#python simpleSimCode/scripts/tempAbove_x_TNY.py  > tempData/diff_dA_da.x_T.NY.txt
+# make data for plots of d_A-d_a
+python simpleSimCode/scripts/tempAbove_x_T.py  > tempData/diff_dA_da.x_T.txt
+python simpleSimCode/scripts/tempAbove_x_TNY.py  > tempData/diff_dA_da.x_T.NY.txt
 
 # simulate pop-gen for different T thresholds
 # first argument is dx_T, difference in UCT between morphs 
 # second argument is rate per yaer of increase in mean temp (in degrees C)
-#python simpleSimCode/scripts/sim.py  0.5 0.05 noFlip > simData/sim.dx_T.0.5.d.0.05.noFlip.txt
-#python simpleSimCode/scripts/sim.py  0.5 0.05 Flip > simData/sim.dx_T.0.5.d.0.05.Flip.txt
+python simpleSimCode/scripts/sim.py  0.5 0.05 noFlip > simData/sim.dx_T.0.5.d.0.05.noFlip.txt
+python simpleSimCode/scripts/sim.py  0.5 0.05 Flip > simData/sim.dx_T.0.5.d.0.05.Flip.txt
 
 python simpleSimCode/scripts/sim.py 1 0.05 noFlip > simData/sim.dx_T.1.d.0.05.noFlip.txt
 python simpleSimCode/scripts/sim.py 1 0.05 Flip > simData/sim.dx_T.1.d.0.05.Flip.txt
 
 # simulate pigeon populations
-#python scripts/simPigeonPop.py 0.02 /Users/telemacher/projects/Pigeon/tempData/Phoenix.GHCND:USW00023183.tMax.csv > simData/sim.Pigeon.d.0.02.Phoenix.txt
-#python scripts/simPigeonPop.py 0.1 /Users/telemacher/projects/Pigeon/tempData/Phoenix.GHCND:USW00023183.tMax.csv  > simData/sim.Pigeon.d.0.1.Phoenix.txt
-#python scripts/simPigeonPop.py 0.02 /Users/telemacher/projects/Pigeon/tempData/Manhattan.GHCND:USW00094728.tMax.csv > simData/sim.Pigeon.d.0.02.Manhattan.txt
-#python scripts/simPigeonPop.py 0.1 /Users/telemacher/projects/Pigeon/tempData/Manhattan.GHCND:USW00094728.tMax.csv > simData/sim.Pigeon.d.0.1.Manhattan.txt
+python scripts/simPigeonPop.py 0.02 tempData/Phoenix.GHCND:USW00023183.tMax.csv > simData/sim.Pigeon.d.0.02.Phoenix.txt
+python scripts/simPigeonPop.py 0.1 tempData/Phoenix.GHCND:USW00023183.tMax.csv  > simData/sim.Pigeon.d.0.1.Phoenix.txt
+python scripts/simPigeonPop.py 0.02 tempData/Manhattan.GHCND:USW00094728.tMax.csv > simData/sim.Pigeon.d.0.02.Manhattan.txt
+python scripts/simPigeonPop.py 0.1 tempData/Manhattan.GHCND:USW00094728.tMax.csv > simData/sim.Pigeon.d.0.1.Manhattan.txt
 
